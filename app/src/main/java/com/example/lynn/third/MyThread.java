9f1a3ce05c11d904593233e3571dbec3193344eb
Computer Lab <complab@asms.net>
2019-06-19 09:21:33 -0500

    Commit

---
+++ app/src/main/java/com/example/lynn/third/MyThread.java
@@ -0,0 +1,13 @@
+package com.example.lynn.third;
+
+public class MyThread implements Runnable {
+
+    public MyThread() {
+        Thread thread =
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
