65dd26ea8716c5de1b616fbdaabde6361846bd20
Computer Lab <complab@asms.net>
2019-06-19 09:27:32 -0500

    Commit

--- app/src/main/java/com/example/lynn/third/MyThread.java
+++ app/src/main/java/com/example/lynn/third/MyThread.java
@@ -3,7 +3,17 @@ package com.example.lynn.third;
 public class MyThread implements Runnable {
 
     public MyThread() {
-        Thread thread =
+        Thread thread = new Thread(this);
+
+        thread.start();
+    }
+
+    private void pause(double seconds) {
+        try {
+            Thread.sleep((int)(seconds*1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
