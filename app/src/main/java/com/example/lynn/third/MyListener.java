c4e404acb5acf64192cf48a015958cf2d806ffb8
Computer Lab <complab@asms.net>
2019-06-19 09:07:49 -0500

    Commit

--- app/src/main/java/com/example/lynn/third/MyListener.java
+++ app/src/main/java/com/example/lynn/third/MyListener.java
@@ -3,6 +3,8 @@ package com.example.lynn.third;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.core.content.ContextCompat;
+
 import static com.example.lynn.third.MainActivity.*;
 
 public class MyListener implements View.OnClickListener {
@@ -11,11 +13,16 @@ public class MyListener implements View.OnClickListener {
     public void onClick(View view) {
         ImageView source = (ImageView)view;
 
-        int id = Integer.parseInt()
+        int id = Integer.parseInt(source.getTag().toString());
 
         if (first == null) {
             first = source;
 
+            first.setImageDrawable(ContextCompat.getDrawable(source.getContext(),id));
+        } else {
+            second = source;
+
+            second.setImageDrawable(ContextCompat.getDrawable(source.getContext(),id));
 
         }
 
